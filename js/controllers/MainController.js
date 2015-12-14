8dafb84be8dfc0462b0520f977223e62b9265e69
schafezp <schafezp@rose-hulman.edu>
2015-12-14 08:19:32 -0500

    Add property scheduleSessions to $scope. Loop through scheduleSessions in index.html and display json as string.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,75 @@
-app.controller('MainController', [ '$scope', function ($scope) {
-  $scope.name = "Test name";
+app.controller('MainController', ['$scope', function ($scope) {
+    $scope.name = "Test name";
+    $scope.scheduleSessions = [
+        {
+            week: 1, // value comes from a session object
+            session: 2, // value comes from a session object
+            sessionDate: 'Wednesday Dec 2', // value comes from a session object
+            scheduleComponents: [
+                {
+                    name: "Topics",
+                    values: [
+                        "Review of course syllabus",
+                        "Brief introduction to Express.js",
+                        "MongoDB installation",
+                        "Getting started with MongoDB"
+                    ]
+                },
+                {
+                    name: "Resources",
+                    values: [
+                        '<a href="../Slides/Introductions.pdf">Slides</a>',
+                        '<a href="http://expressjs.com/starter/installing.html">Express Installation</a>',
+                        '<a href="https://docs.mongodb.org/manual/installation/">MongoDB installation</a>'
+                    ]
+                },
+                {
+                    name: "Reading",
+                    values: [
+                        '<a href="../syllabus.html">Course Syllabus</a>',
+                        '<a href="https://docs.mongodb.org/manual/">MongoDB Documentation</a>',
+                        ' <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript">JavaScript Primer</a>',
+                        '<a href="../Assignments/HelloWorldExpressExample.pdf">Hello World Express Exampla</a>',
+                        '<a href="../Assignments/GettingStartedWithMongoDB.pdf">Getting Started with MongoDB</a>'
+                    ]
+                }
+            ]
+        }
+        ,
+        {
+            week: 1, // value comes from a session object
+            session: 2, // value comes from a session object
+            sessionDate: 'Wednesday Dec 2', // value comes from a session object
+            scheduleComponents: [
+                {
+                    name: "Topics",
+                    values: [
+                        "Review of course syllabus",
+                        "Brief introduction to Express.js",
+                        "MongoDB installation",
+                        "Getting started with MongoDB"
+                    ]
+                },
+                {
+                    name: "Resources",
+                    values: [
+                        '<a href="../Slides/Introductions.pdf">Slides</a>',
+                        '<a href="http://expressjs.com/starter/installing.html">Express Installation</a>',
+                        '<a href="https://docs.mongodb.org/manual/installation/">MongoDB installation</a>'
+                    ]
+                },
+                {
+                    name: "Reading",
+                    values: [
+                        '<a href="../syllabus.html">Course Syllabus</a>',
+                        '<a href="https://docs.mongodb.org/manual/">MongoDB Documentation</a>',
+                        ' <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript">JavaScript Primer</a>',
+                        '<a href="../Assignments/HelloWorldExpressExample.pdf">Hello World Express Exampla</a>',
+                        '<a href="../Assignments/GettingStartedWithMongoDB.pdf">Getting Started with MongoDB</a>'
+                    ]
+                }
+            ]
+        }
+    ];
+    $scope.myname = "my name"
 }]);
