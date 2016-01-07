0ced122fd3a038e84ba9bef2cb54d6a8e447df08
Zachary Schafer <zachary.schafer@me.com>
2016-01-07 14:06:46 -0500

    Updated generator to include labs. In schedule page links to labs are broken.

--- js/generate-schedule/generateconfig.js
+++ js/generate-schedule/generateconfig.js
@@ -35,9 +35,34 @@ var homework = [
         sample: '',
     }
 ];
+var labs = [
+    {
+        type: "lab",
+        session: "1",
+        name: "Lab 1",
+        number: 1,
+        sessionDue: 3,
+        url: "lab-1.html",
+        solution: "",
+        sample: ""
+    },
+    {
+        type: "lab",
+        session: "2",
+        name: "Lab 2",
+        number: 2,
+        sessionDue: 4,
+        url: "lab-2.html",
+        solution: "",
+        sample: ""
+    }
+]
 
 var courseComponents = [];
-courseComponents = courseComponents.concat(homework)
+
+//ADD ANY NEW COURSE COMPONENTS HERE ---
+courseComponents = courseComponents.concat(homework, labs)
+//----
 var scheduleComponentNames = ["Due", "Topics", "Resources", "Reading", "Programs", "Whatever"]
 var updatedConfig = new Config("MWR", "11/30/2015", 1, "12/21/2015", "1/4/2016", 30, scheduleComponentNames, courseComponents);
 
