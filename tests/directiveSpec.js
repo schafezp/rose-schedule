4669cf003818206228a0bb8a4a8f1e6ec67b17ee
asleeponduty <lukedrong@gmail.com>
2016-02-01 15:12:13 -0500

    Finished writing tests

--- tests/directiveSpec.js
+++ tests/directiveSpec.js
@@ -1,18 +1,20 @@
 describe('Rose Schedule Directives',function(){
 
     beforeEach(angular.mock.module('schedule'));
-    
-    describe('template',function(){
+    //load the templates
+    beforeEach(module('templates'));    
+
+    describe('list assignments: template',function(){
       //load module that contains the directives  
       
 
-        //load the templates
+
       //beforeEach(module('src/views/listAssignments.html'));
 
       var $compile, $scope, $httpBackend;
 
       //Load the templates module
-      beforeEach(module('templates'));
+
 
       beforeEach(inject( function( _$compile_,_$rootScope_, _$httpBackend_){
           $compile = _$compile_;
@@ -73,8 +75,70 @@ describe('Rose Schedule Directives',function(){
 
       }));
 
+    });
+
+    describe('session table row: template',function(){
+      var $compile, $scope, $httpBackend, $sce ;
+
+      beforeEach(inject( function( _$compile_,_$rootScope_, _$httpBackend_, _$sce_){
+          $compile = _$compile_;
+          $scope = _$rootScope_.$new();
+          $httpBackend = _$httpBackend_;
+          $sce = _$sce_
+          //Do not respond to 'schedule' module request for Schedule.json
+          $httpBackend.whenGET('src/json/schedule.json').respond("");
+
+
+      } ));
+
+      
+
+      it("should render the session date of the row, passed in by $scope", inject(function(){
           
-    
+          var template = $compile("<tr student-table-row></tr>")($scope);
+          $scope.componentNames = [
+            ];
+          $scope.session = {
+            "sessionNumber": 1,
+            "sessionWeekDay": "Monday",
+            "sessionDate": "11/30/2015",
+            "week": 1
+          };
+
+          $scope.$digest();
+
+          //Render the template as a string. 
+          var templateAsHtml = template.html(); 
+          expect(templateAsHtml).toContain($scope.session.sessionDate);
+
+      }));
+
+      it("should properly set the components as columns", inject(function(){
+          var template = $compile("<tr student-table-row></tr>")($scope);
+          $scope.componentNames = [
+            "Due"
+          ];
+          $scope.session = {
+            "sessionNumber": 1,
+            "sessionWeekDay": "Monday",
+            "sessionDate": "11/30/2015",
+            "week": 1,
+            "scheduleComponents": [
+              {
+                "name": "Due",
+                "values": ["Homework"]
+              }
+            ]
+          };
+          $scope.to_trusted = function (html_code) {
+            return $sce.trustAsHtml(html_code);
+          };
+          $scope.$digest();
+
+          //Render the template as a string. 
+          var templateAsHtml = template.html(); 
+        expect(templateAsHtml).toContain($scope.session.scheduleComponents[0].values[0]);
+      } ));
 
     });
 });
